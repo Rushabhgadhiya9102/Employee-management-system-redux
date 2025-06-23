2d3a29cdc6afa8f6fc879ccf62803594210e518a
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-06-23 20:50:51 +0530

    done;

--- src/pages/userpanel/UserDashboard.jsx
+++ src/pages/userpanel/UserDashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { logoutUser } from '../../features/auth/authSLice'
+import { logoutUser } from '../../features/auth/authSlice'
 
 const UserDashboard = () => {
 
